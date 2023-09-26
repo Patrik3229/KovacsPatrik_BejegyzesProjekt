7ef57c9271f5002d30eda7b4f9529030d45ee396
Kovács Patrik <kovacs.patrik@petrik.hu>
2023-09-26 08:59:16 +0200

    2a

--- BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/Program.cs
@@ -8,9 +8,19 @@ namespace BejegyzesProjekt
 {
     internal class Program
     {
-        
+        static List<Bejegyzes> lista1 = new List<Bejegyzes>();
+        static List<Bejegyzes> lista2 = new List<Bejegyzes>();
+
+        static void Feladat2()
+        {
+            
+        }
+
+
         static void Main(string[] args)
         {
+            Feladat2();
+
             Console.ReadKey();
         }
     }
