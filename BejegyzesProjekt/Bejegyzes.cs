9e52b449f8fbc3896a41334b3d015ceaad588d72
Patrik <Patrik@DESKTOP-FIVBINU>
2023-09-26 08:36:09 +0200

    Add project files.

---
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    internal class Bejegyzes
+    {
+        private string szerzo;
+        private string tartalom;
+        private int likeok;
+        private DateTime letrejött;
+        private DateTime szerkesztve;
+
+        public string Szerzo { get => szerzo;}
+        public string Tartalom { get => tartalom; set => tartalom = value; }
+        public int Likeok { get => likeok;}
+        public DateTime Letrejött { get => letrejött;}
+        public DateTime Szerkesztve { get => szerkesztve;}
+    }
+}
